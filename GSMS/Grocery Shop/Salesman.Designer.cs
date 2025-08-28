23f7a349d432c4bbbfacb80751c9e50f85835f7b
mijbahur <hacinrahman03@gmail.com>
2025-08-28 12:58:04 +0600

    Project added

---
+++ GSMS/Grocery Shop/Salesman.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Grocery_Shop
+{
+    partial class Salesman
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Salesman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Salesman";
+            this.Text = "Salesman";
+            this.Load += new System.EventHandler(this.Salesman_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ GSMS/Grocery Shop/Salesman.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grocery_Shop
+{
+    public partial class Salesman : Form
+    {
+        public Salesman()
+        {
+            InitializeComponent();
+        }
+
+        private void Salesman_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
