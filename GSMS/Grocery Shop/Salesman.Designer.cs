6c99d50be96dc42fa3394b3e11c96cbeacda015f
mijbahur <hacinrahman03@gmail.com>
2025-08-28 13:08:22 +0600

    Guna UI added

--- GSMS/Grocery Shop/Salesman.Designer.cs
+++ GSMS/Grocery Shop/Salesman.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Salesman";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Salesman";
             this.Load += new System.EventHandler(this.Salesman_Load);
             this.ResumeLayout(false);
